dde3f0543b2e4c0f1aef04166caaa21f72c07009
vikasgautam1234 <vikasmtr1234@gmail.com>
2023-05-13 02:44:12 +0530

    navbar and routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 
 import './App.css';
 import {Routes,Route} from 'react-router-dom'    //Routes is a cointainer and we can perform routing with the help of it.
+import { useMemo } from 'react';
+import { themeSettings } from './theme';
+import { CssBaseline, ThemeProvider} from '@mui/material'
+import { createTheme } from '@mui/material/styles';
+
 import Navbar from './components/Navbar'
 import Homepage from './pages/Homepage'
 import Login from './pages/Login'
 import Register from './pages/Register'
 function App() {
+  const theme = useMemo(()=>createTheme(themeSettings(), []))
   return(
     <>
+    <ThemeProvider theme = {theme}>
+      <CssBaseline/>
     <Navbar/>
     <Routes>
       {/* with the help of ELEMENT we can show the component  */}
@@ -15,6 +23,7 @@ function App() {
       <Route path='register' element = {<Register/>}/>
       <Route path='login' element = {<Login/>}/>
     </Routes>
+    </ThemeProvider>
 
     </>
   )
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,22 +1,23 @@
 import React from 'react'
-import { Box, Typography} from '@mui/material'
-import { NavLink } from 'react-router-dom'
+import { Box, Typography, useTheme,Link} from '@mui/material'
 const Navbar = () => {
+  const theme = useTheme()
   return (
     <Box width = {"100%"}
+    backgroundColor = {theme.palette.background.alt}
     p= "1rem 6%"
      textAlign ={"center"}
-     sx = {{boxShadow: 3, mb:2, bgcolor: "purple", color: "white"}}
+     sx = {{boxShadow: 3, mb:2}}
      >
-      <Typography variant = "h1" color = {"white"} fontWeight={"bold"}>
+      <Typography variant = "h1" color = {"primary"} fontWeight={"bold"}>
           AI GPT 2 clone
       </Typography>
-      <NavLink to = "/register" p = {1}>
+      <Link href = "/register" p = {1}>
         Sign UP
-      </NavLink>
-      <NavLink to = "/login" p= {1}>
+      </Link>
+      <Link href = "/login" p= {1}>
         Sign In
-      </NavLink>
+      </Link>
     </Box>
   )
 }
